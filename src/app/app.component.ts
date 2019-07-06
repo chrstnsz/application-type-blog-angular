fd18753ca0238860eb5f59b80f9cd51d6787446e
chrstnsz <mytraxx@outlook.com>
2019-07-06 10:00:36 +0200

    Update 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet-angular2';
+
+  posts = [
+    {
+      title: 'Premier',
+      content: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. Do eiusmod tempor incididunt ut labore et dolore magna aliqua. Esse cillum dolore eu fugiat nulla pariatur. Et harum quidem rerum facilis est et expedita distinctio.',
+      loveIts: 0,
+      created_at: 'Saturday July 06 2019 16:33'
+    },
+    {
+      title: 'Deuxieme',
+      content: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. Do eiusmod tempor incididunt ut labore et dolore magna aliqua. Esse cillum dolore eu fugiat nulla pariatur. Et harum quidem rerum facilis est et expedita distinctio.',
+      loveIts: 0,
+      created_at: 'Saturday July 06 2019 16:34'
+    },
+    {
+      title: 'Troisieme',
+      content: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. Do eiusmod tempor incididunt ut labore et dolore magna aliqua. Esse cillum dolore eu fugiat nulla pariatur. Et harum quidem rerum facilis est et expedita distinctio.',
+      loveIts: 0,
+      created_at: 'Saturday July 06 2019 16:36'
+    },
+  ];
 }
